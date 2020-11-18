f2c348e693077b3cc9d935bc850f4329714e9f5e
NastyaUsoltseva <72483918+NastyaUsoltseva@users.noreply.github.com>
2020-11-18 09:17:51 +0300

    Add files via upload

---
+++ labwork3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<stdio.h>
+int main()
+{
+	int array2[10];
+	int array[10];
+	printf("Enter values of arrray");
+	for (int i = 0;i < 10;i++)
+	{
+		scanf_s("%d", &array[i]);
+	}
+	for (int i = 0;i < 10;i++)
+	{
+		printf("%d", array[i]);
+	}
+	for (int i = 0;i < 10;i++)
+	{
+		if (i <= 0)
+		{
+			array2[10] = i;
+		}
+	}
+	for (int i = 0;i < 10;i++)
+	{
+		printf("%d", array2[i]);
+	}
+	return 0;
+}
